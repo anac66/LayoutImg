b1b2b1b6c4d2d0f9800a2a550d3099794432527a
anac66 <karlasmith-hayley@live.com>
2022-06-28 17:38:29 -0300

    Layout

---
+++ App.js
@@ -0,0 +1,17 @@
+App.js
+import { StatusBar } from 'expo-status-bar';
+import { View } from 'react-native';
+import { Exercicio } from './src/components/flex/exercicios';
+
+export default function App () {
+
+    return (
+        <View style={{flex:1}}>
+
+        <Exercicio />
+        <StatusBar style="auto" />
+        </View>
+
+    );
+};
+index.js
